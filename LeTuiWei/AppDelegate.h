f65f43e415f5ecb59c299fe4570c953a36690281
陈营超 <chenyingchao@EDZdeMacBook-Pro.local>
2017-03-28 15:43:32 +0800

    Initial Commit

---
+++ LeTuiWei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeTuiWei
+//
+//  Created by 陈营超 on 2017/3/28.
+//  Copyright © 2017年 陈营超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
